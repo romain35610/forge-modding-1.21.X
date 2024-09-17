676cb5d529f7e8c043fb8995ec2e74cd2b2c028c
romain35610 <93052656+romain35610@users.noreply.github.com>
2024-09-17 17:34:39 +0200

    ajout red emerald (ne fonctionne pas et jsp pourquoi

--- src/main/java/com/romain35610/premiermod/PremierMod.java
+++ src/main/java/com/romain35610/premiermod/PremierMod.java
@@ -1,6 +1,8 @@
 package com.romain35610.premiermod;
 
 import com.mojang.logging.LogUtils;
+import com.romain35610.premiermod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,7 +31,7 @@ public class PremierMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -43,7 +45,9 @@ public class PremierMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.RED_EMERALD);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/romain35610/premiermod/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.romain35610.premiermod.item;
+
+import com.romain35610.premiermod.PremierMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, PremierMod.MOD_ID);
+
+    public static final RegistryObject<Item> RED_EMERALD = ITEMS.register("red_emerald",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
